f8e3fbd974f8f776b26c49a6c0672958181af2ed
Luiz Filipe Magalhães Freitas <felipe22331@gmail.com>
2026-08-27 08:07:53 -0300

    refatoracao

--- IRP_WW.py
+++ IRP_WW.py
@@ -1,53 +1,49 @@
 def wagner_whitin(d, s, h):
     
-    N = len(d) 
-
-    H = [0] * (N + 1)
+    T = len(d) 
+    H = [0] * (T + 1)
     
-    for i in range(N):
-        H[i+1] = H[i] + h # Trata o custo de armazenamento como constante 
+    for i in range(T):
+        H[i+1] = H[i] + h # Trata o custo de armazenamento somente na planta
 
     # F[t] = custo mínimo até t
-    F = [0] * N
-    parent = [0] * N
-
-    # Função custo
-    def cost(j, t):
-        total = s
-        for k in range(j+1, t+1):
-            holding = H[k] - H[j]  # custo acumulado de h[j] até h[k-1]
-            total += d[k] * holding # mais custo indireto
-        return total
-
+    F = [0] * T
+    parent = [0] * T
     
-    for t in range(N):
+    for t in range(T):
         F[t] = float('inf')
         for j in range(t + 1):
             if j == 0:
-                c = cost(0, t)
+                c = cost(0, t, d, s, H)
             else:
-                c = F[j-1] + cost(j, t)
+                c = F[j-1] + cost(j, t, d, s, H)
 
             if c < F[t]:
                 F[t] = c
                 parent[t] = j
 
     # reconstrução
-    pedidos = [0] * N
-    t = N - 1
+    pedidos = [0] * T
+    t = T - 1
 
     while t >= 0:
         j = parent[t]
         pedidos[j] = sum(d[k] for k in range(j, t+1))
         t = j - 1
 
-    return F[N-1], pedidos
+    return F[T-1], pedidos
+
+# Função custo
+def cost(j, t, d, s, H):
+    total = s
+    for k in range(j+1, t+1):
+        holding = H[k] - H[j]
+        total += d[k] * holding
+    return total
 
 def main(d, s, h):
 
     print("INICIANDO WW...\n\n")
-
-    
 
     custo, pedidos = wagner_whitin(d, s, h)
 
--- main.py
+++ main.py
@@ -3,33 +3,31 @@ import IRP_WW
 import pyVRP
 
 
-clientes, N, T = leitura.data() # Utiliza o modulo de leitura para obter os dados dos clientes
+#Leitura dos dados
+clientes, N, T = leitura.data()
 
+#Inicialização de variáveis
 demanda_ww = []
-
 custo_setup_ww = clientes[0].s
 
-for j in range(0,T):
+for i in range(0,T):
     aux = 0
-    
-    for i in clientes[1:N+1]:
-        aux += i.d[j]    
-    
-    demanda_ww.append(aux) # Cria um vetor de demanda para o WW a partir dos dados dos clientes
-    
 
-custo, pedidos = IRP_WW.main(demanda_ww, custo_setup_ww, i.h)
+    for j in clientes[1:N+1]:
+        aux += j.d[i]        
 
-demanda_ww = [] # Limpa o vetor de demanda para o próximo cliente
-
-    
-
-# Chamada do pyVRP
-
-pyVRP.main(clientes[0], clientes[1:N+1], pedidos, T)
-
-#ADICIONAR CUSTO TOTAL (PRODUCAO + SETUP + TRANSPORTE) E CUSTO DE ARMAZENAMENTO (HOLDING COST)
-    
+    demanda_ww.append(aux)
 
 
+def main():
+    custo, pedidos = IRP_WW.main(demanda_ww, clientes[0].s, clientes[0].h)
 
+
+    # Chamada do pyVRP
+
+    #pyVRP.main(clientes[0], clientes[1:N+1], pedidos, T)
+
+    #ADICIONAR CUSTO TOTAL (PRODUCAO + SETUP + TRANSPORTE) E CUSTO DE ARMAZENAMENTO (HOLDING COST)
+        
+main()
+demanda_ww = []
